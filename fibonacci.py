bddea15ab641f5899d56e44c1f0b08a36174d2ef
Sergey <xxsmotur@gmail.com>
2019-02-03 16:05:52 +0500

    Fixing of initialization

--- fibonacci.py
+++ fibonacci.py
@@ -13,22 +13,22 @@ class FibonacciHeap:
         '''
         self._nodes+=1
         if not self.head:
-            self.head = Node()
+            self.head = Node(x)
             return
         new_node = Node(x)
         new_node.left = self.head
         new_node.right = self.head.right
         self.head.right = new_node
         if new_node.right:
-            new_node.right.left = node
+            new_node.right.left = new_node
         else:
             new_node.right = self.head
             self.head.left = new_node
         
-        if new_node.key > self.head.key:
+        if new_node.value < self.head.value:
             self.head = new_node
     
     def min(self):
         ''' return head of the heap with minimal value
         '''
-        return self.head.key
+        return self.head.value
