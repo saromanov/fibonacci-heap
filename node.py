a8a0289b6195201f2ab1eedc5bd106ef987c0e2d
Sergey <xxsmotur@gmail.com>
2019-02-03 15:56:57 +0500

    adding value to the node

--- node.py
+++ node.py
@@ -1,10 +1,11 @@
 class Node:
     ''' implementation of node for fibonacci heap
     '''
-    def __init__(self):
+    def __init__(self, value):
         self.left = None
         self.right = None
         self.child = None
         self.parent = None
         self.degree = 0
         self.mark = False
+        self.value = value
