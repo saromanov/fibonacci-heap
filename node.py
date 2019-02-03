8a741e68b12f8da499190c0cb4f4033c703edcb8
Sergey <xxsmotur@gmail.com>
2019-02-03 15:50:16 +0500

    Add Node class

---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+    ''' implementation of node for fibonacci heap
+    '''
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.child = None
+        self.parent = None
+        self.degree = 0
+        self.mark = False
